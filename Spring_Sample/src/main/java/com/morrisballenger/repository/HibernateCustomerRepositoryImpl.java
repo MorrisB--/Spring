eac357e2b0e069ed95dc8b619d3464c5a4e866a4
MorrisB-- <morris.ballenger@yahoo.com>
2016-12-17 11:26:04 -0600

    Created Application to test

--- Spring_Sample/src/main/java/com/morrisballenger/repository/HibernateCustomerRepositoryImpl.java
+++ Spring_Sample/src/main/java/com/morrisballenger/repository/HibernateCustomerRepositoryImpl.java
@@ -1,5 +1,26 @@
 package com.morrisballenger.repository;
 
-public class HibernateCustomerRepositoryImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import com.morrisballenger.model.Customer;
+
+public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+
+	/* (non-Javadoc)
+	 * @see com.morrisballenger.repository.CustomerRepository#findAll()
+	 */
+	@Override
+	public List<Customer> findAll() {
+		List<Customer> customers = new ArrayList<>();
+		Customer customer = new Customer();
+
+		customer.setFirstName("FirstName0");
+		customer.setLastName("LastName0");
+		
+		customers.add(customer);
+
+		return customers;
+	}
 
 }
