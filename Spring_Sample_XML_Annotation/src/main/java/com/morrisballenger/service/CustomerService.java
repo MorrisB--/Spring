848c89ed2e9920d27ded712807be8a5d9057abf1
MorrisB-- <morris.ballenger@yahoo.com>
2016-12-18 11:56:32 -0600

    Created test project

---
+++ Spring_Sample_XML_Annotation/src/main/java/com/morrisballenger/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.morrisballenger.service;
+
+import java.util.List;
+
+import com.morrisballenger.model.Customer;
+
+public interface CustomerService {
+
+	List<Customer> findAll();
+
+}
